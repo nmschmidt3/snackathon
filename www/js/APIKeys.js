f7fa69ccdfcb8719d42abac0f3359cb44022de1e
Nate Schmidt <nate.schmidt.2013@gmail.com>
2017-07-14 03:37:49 -0500

    first commit

---
+++ www/js/APIKeys.js
@@ -0,0 +1,17 @@
+/*
+ * IndoorAtlas Cordova Plugin Examples
+ * https://github.com/IndoorAtlas/cordova-plugin
+ * https://github.com/IndoorAtlas/sdk-cordova-examples
+ */
+
+// API KEYS FOR INDOORATLAS SERVICES AND GOOGLE MAPS JAVASCRIPT API
+
+// Get Google Maps API Key from here: https://console.developers.google.com/apis/credentials
+var GOOGLE_API_KEY = 'AIzaSyDGAC6ToJ7nVUFlFl6oawDVmcWOmeA-yKE';
+
+// Get IndoorAtlas API Key and Secret from here https://app.indooratlas.com/apps
+var IA_API_KEY = '00c68557-4a51-41cc-afde-ecb1d7ca4536';
+var IA_API_SECRET = 'EerXH48akIeUhUKdNaYFN2DKhlpAb3lT9r9Bd1icQMM6P9m03XduRFF9gqaa2EEFVYHIaBHfbrU+aOoFc0anb4vScFcx2VyNGej/dQAef43tauw6FHtwTqLXK89o4A==';
+
+// Floorplan ID https://developer.indooratlas.com/venues
+var IA_FLOORPLAN_ID = '08969f7a-cf15-4bfb-abf0-ad7ef494fea7';
